df4f00bf210baf245d506fb713a705819b4301e5
YvonneJL <yvonneloder@gmx.de>
2024-12-10 14:42:12 +0100

    fertig

---
+++ Aufgabe_6/assets/js/main.js
@@ -0,0 +1,23 @@
+const derBody = document.querySelector(".change-background-color");
+const dieHeadline = document.querySelector(".change-color");
+
+
+function changeOne() {
+    derBody.style.backgroundColor = "red";
+    dieHeadline.style.color = "blue";
+}
+
+function changeTwo() {
+    derBody.style.backgroundColor = "orange";
+    dieHeadline.style.color = "teal";
+}
+
+function changeThree() {
+    derBody.style.backgroundColor = "antiquewhite";
+    dieHeadline.style.color = "purple";
+}
+
+function changeFour() {
+    derBody.style.backgroundColor = "yellowgreen";
+    dieHeadline.style.color = "orange";
+}
